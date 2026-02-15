5206b8701a0a61bb5d54e07a71564f3c3a3da6dc
Gaurav Sen <gaurav.s3n@gmail.com>
2026-02-15 17:07:09 +0530

    focus-flow initial setup

---
+++ Async/11 - Final Project/focus-flow/src/main.js
@@ -0,0 +1,6 @@
+import "./style.css"
+import { setupSessionCreation } from "./create-session.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  setupSessionCreation(document.querySelector("#focus-flow #create-session-form"));
+})
