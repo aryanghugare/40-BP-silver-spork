bd6a4e32fa41222a4f01a0ccdfc907345106cc29
Gaurav Sen <gaurav.s3n@gmail.com>
2026-02-21 13:15:50 +0530

    session history rendering

--- Async/11 - Final Project/focus-flow/src/main.js
+++ Async/11 - Final Project/focus-flow/src/main.js
@@ -6,6 +6,29 @@ import { readFromStorage } from "./utils/storage.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const currentSession = readFromStorage(STORAGE_KEYS.CURRENT_SESSION);
+  const historySection = document.getElementById("history");
+  const container = document.createElement("ul");
+  historySection.append(container);
+
+  const sessionHistory = readFromStorage(STORAGE_KEYS.SESSION_HISTORY);
+
+  for (let { name, time, status } of sessionHistory) {
+    const historyItem = document.createElement("li");
+    const titleElement = document.createElement("span");
+    titleElement.textContent = name;
+
+    const timeElement = document.createElement("span");
+    timeElement.textContent = time;
+
+    const statusElement = document.createElement("span");
+    statusElement.textContent = status;
+
+    historyItem.append(titleElement, timeElement, statusElement);
+
+    container.append(historyItem);
+
+  }
+
   if (currentSession) {
     startSession(currentSession);
   } else {
