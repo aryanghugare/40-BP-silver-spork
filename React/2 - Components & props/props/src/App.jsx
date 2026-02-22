d17f0e4f615c4caca8876428249ce82c52695598
Gaurav Sen <gaurav.s3n@gmail.com>
2026-02-22 11:53:50 +0530

    Day 2 react folder structure modification

---
+++ React/2 - Components & props/props/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <section>content</section>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
