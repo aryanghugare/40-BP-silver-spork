7648beb904f59bcab214baa46c0e3c5ad05ba866
Gaurav Sen <gaurav.s3n@gmail.com>
2026-02-28 12:49:27 +0530

    Lifting the state - input sync example

--- React/5 - Lifting the state up/state-patterns/src/App.jsx
+++ React/5 - Lifting the state up/state-patterns/src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import Accordion from "./component/accordion";
+import Input from "./component/input";
 
 function App() {
-  return <Accordion title="Accordion Title" />;
+  const [value, setValue] = useState("");
+  return (
+    <>
+      <Accordion title="Accordion Title" />;
+      <Input value={value} onChange={setValue} />
+      <Input value={value} onChange={setValue} />
+    </>
+  );
 }
 
 export default App;
---
+++ React/5 - Lifting the state up/state-patterns/src/component/input.jsx
@@ -0,0 +1,4 @@
+import { useState } from "react";
+export default function Input({ value, onChange }) {
+  return <input type="text" value={value} onChange={(e) => onChange(e.target.value)} />;
+}
