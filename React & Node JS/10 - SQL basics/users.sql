090d075bf2ae28f87d55393a6531cdefab54c005
Gaurav Sen <gaurav.s3n@gmail.com>
2026-03-13 18:53:07 +0530

    examples of filtering

--- React & Node JS/10 - SQL basics/users.sql
+++ React & Node JS/10 - SQL basics/users.sql
@@ -33,3 +33,32 @@
 
 select name, email, created_at from users;
 
+
+
+-- alter table <table_name> add column <column_name> <data-type>
+
+-- alter table users add column age integer default 0;
+
+-- update users set age = 18 where id = 2;
+-- update users set age = 30 where id = 4;
+
+
+-- filtering queries
+-- where clause
+-- select all users whose age is greater than 20
+select * from users where age > 20;
+
+select * from users where name ILIKE 'Rahul'; -- ilike is case insensitive
+
+select * from users where name ILIKE '%r%';  
+
+-- order by
+
+-- select all users arrange the results by age in ascending order
+select * from users order by age asc;
+
+-- select all users order by desc - filter further by limiting the result to single result
+-- return the user whose is the oldest in users
+
+-- limit 
+select * from users order by age desc limit 1;
