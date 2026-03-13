8707a265d3fc2385d5e4d712917b741273773132
Gaurav Sen <gaurav.s3n@gmail.com>
2026-03-13 21:12:13 +0530

    aggregations - sum, count avg, sub queries, group by and having

---
+++ React & Node JS/11 - joins & relationships/aggregations.sql
@@ -0,0 +1,41 @@
+select count(*) from users;
+
+select count(user_id) from posts group by user_id;
+
+-- group by - groups rows that share the same values in the specified column, so that you can run 
+-- aggregate functions on each group separately.
+
+
+select user_id, count(id) from posts
+group by user_id;
+
+select users.name, count(posts.id)
+from users left join posts on posts.user_id = users.id
+group by users.name;
+
+select * from users;
+
+-- insert into users (name, age, email) values ('sheetal', 29, 'sheetal@test.com');
+
+
+select role, count(id) from users group by role;
+
+-- get all users who have more than one post
+select users.name, count(posts.id) as post_count 
+from users 
+left join posts on posts.user_id = users.id
+group by users.id, users.name
+having count(posts.id) > 2;
+
+select name, (select count(*) from posts where posts.user_id = users.id) as post_count
+from users;
+
+select avg(post_count) as avg_posts
+from (
+    select user_id, count(*) as post_count
+    from posts
+    group by user_id
+) as post_summary;
+
+-- get sum of age of all users
+select sum(age) as total_age from users;
