d895806fb5f26fe6ff41b2709e1bbe4f11c9fd6e
Gaurav Sen <gaurav.s3n@gmail.com>
2026-03-13 20:36:36 +0530

    11 - relations and joins

--- React & Node JS/11 - joins & relationships/posts.sql
+++ React & Node JS/11 - joins & relationships/posts.sql
@@ -15,3 +15,17 @@
 -- ('learn sql', 'content related to learn sql', 7);
 
 select * from posts;
+
+select posts.title, users.name from posts inner join users on posts.user_id = users.id;
+
+-- left join - all rows from the left table 
+select posts.title, users.name from posts left join users on posts.user_id = users.id;
+
+-- right join all rows from the right table
+
+select posts.title, users.name from posts right join users on posts.user_id = users.id;
+
+-- INNER JOIN only matching rows in both tables
+-- LEFT JOIN all left rows + matched right rows 
+-- RIGHT JOIN all right rows + matched left rows
+
