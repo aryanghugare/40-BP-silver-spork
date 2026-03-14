e638811bc075bc77ab51bc15808ec1b8e7949b14
Gaurav Sen <gaurav.s3n@gmail.com>
2026-03-14 14:21:19 +0530

    post example

--- React & Node JS/12 - mongodb/db.js
+++ React & Node JS/12 - mongodb/db.js
@@ -1,3 +1,4 @@
+const express = require("express");
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // npm install dotenv in case of older node versions
@@ -12,13 +13,43 @@ const client = new MongoClient(uri, {
     }
 });
 
+let db;
+const app = express();
+app.use(express.json());
+
+async function connect() {
+
+    await client.connect();
+    return client.db();
+
+}
+
+app.post("/posts", async (req, res) => {
+    const { author, ...rest } = req.body;
+    db = await connect();
+    const existingPost = await db.collection("posts").find({ author });
+    if (existingPost) {
+        result = await db.collection("posts").updateOne({
+            author
+        }, {
+            $push: { posts: rest.posts[0] }
+        })
+    } else {
+        result = await db.collection("posts").insertOne({ author, ...rest });
+    }
+    client.close();
+    res.json(result)
+})
+
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
-        const db = await client.db();
+        db = await client.db();
         await db.command({ ping: 1 });
 
         // const result = await db.collection("test").insertOne({
@@ -35,16 +66,21 @@ async function run() {
         // }])
         // console.log(result);
 
-        console.log(await db.collection("test").find({ name: "test2" }).toArray());
+        // console.log(await db.collection("test").find({ name: "test2" }).toArray());
 
         // console.log(await client.db("sample_mflix").collection("students").insertOne({
         //     name: "gaurav",
         //     age: 29, role: "admin", course: "MERN"
         // }))
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
     }
 }
-run().catch(console.dir);
+
+app.listen(3000, () => {
+    console.log("server started successfully");
+    run().catch(console.dir);
+})
