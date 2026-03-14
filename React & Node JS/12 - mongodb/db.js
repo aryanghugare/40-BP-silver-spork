6f6b6ae814cb368df8b91fc96073251e84a8c01c
Gaurav Sen <gaurav.s3n@gmail.com>
2026-03-14 13:13:30 +0530

    insert and find operations

--- React & Node JS/12 - mongodb/db.js
+++ React & Node JS/12 - mongodb/db.js
@@ -1,5 +1,5 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // npm install dotenv in case of older node versions
 const uri = process.env.CONNECTION_STRING;
 
@@ -17,7 +17,30 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
+        const db = await client.db();
+        await db.command({ ping: 1 });
+
+        // const result = await db.collection("test").insertOne({
+        //     name: "test1",
+        //     age: 20
+        // })
+
+        // await db.collection("test").insertMany([{
+        //     name: "test3",
+        //     age: 21
+        // }, {
+        //     name: "test2",
+        //     age: 22
+        // }])
+        // console.log(result);
+
+        console.log(await db.collection("test").find({ name: "test2" }).toArray());
+
+        // console.log(await client.db("sample_mflix").collection("students").insertOne({
+        //     name: "gaurav",
+        //     age: 29, role: "admin", course: "MERN"
+        // }))
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
