95c8d810a3ff56ae6cfa3dbde19fc392aa45edc8
Gaurav Sen <gaurav.s3n@gmail.com>
2026-03-21 11:05:10 +0530

    added color pallete

--- React & Node JS/13 - Minor Project/e-commerce-app/src/components/product/product-color.jsx
+++ React & Node JS/13 - Minor Project/e-commerce-app/src/components/product/product-color.jsx
@@ -1,8 +1,19 @@
 export default function ProductColor({ colorList, onColorSelect }) {
+  const colorPallete = new Map([
+    ["red", "#EF4444"],
+    ["blue", "#3b82f6"],
+    ["green", "#10B981"],
+    ["black", "#2f3d5b"],
+    ["white", "#F9FAFB"],
+  ]);
   return (
     <section className="product-colors">
       {colorList?.map((color) => (
-        <button key={color} onClick={() => onColorSelect(color)} style={{ backgroundColor: color }}></button>
+        <button
+          key={color}
+          onClick={() => onColorSelect(color)}
+          style={{ backgroundColor: colorPallete.get(color) }}
+        ></button>
       ))}
     </section>
   );
