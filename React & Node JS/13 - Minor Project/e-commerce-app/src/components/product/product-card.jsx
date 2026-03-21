19c64d59aad2d9b709e58d3dcb4a6d1b2f35b092
Gaurav Sen <gaurav.s3n@gmail.com>
2026-03-21 13:25:52 +0530

    added product description as tooltip

--- React & Node JS/13 - Minor Project/e-commerce-app/src/components/product/product-card.jsx
+++ React & Node JS/13 - Minor Project/e-commerce-app/src/components/product/product-card.jsx
@@ -20,7 +20,9 @@ export default function ProductCard({ product }) {
         <img className="product-image" src={product.image} alt={product.title} />
       </header>
       <h3 className="product-title truncate">{product.title}</h3>
-      <p className="product-description truncate-2">{product.description}</p>
+      <p title={product.description} className="product-description truncate-2">
+        {product.description}
+      </p>
       <p className="product-price">${product.price.toFixed(2)}</p>
       <ProductSize sizeList={product.size} onSizeSelect={setSize} />
       <ProductColor colorList={product.color} onColorSelect={setColor} />
