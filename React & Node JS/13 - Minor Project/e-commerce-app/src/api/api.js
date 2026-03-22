c181fd8a882d4008f5f6c4c411d97e91235a9738
Gaurav Sen <gaurav.s3n@gmail.com>
2026-03-22 13:12:34 +0530

    added logic to call api endpoints related to cart functionality

--- React & Node JS/13 - Minor Project/e-commerce-app/src/api/api.js
+++ React & Node JS/13 - Minor Project/e-commerce-app/src/api/api.js
@@ -4,3 +4,26 @@ export async function fetchProducts() {
     const response = await fetch(backendURL + "/products");
     return response.json();
 }
+
+export async function addToCart(cartItem) {
+    const result = await fetch(`${backendURL}/cart`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(cartItem)
+    });
+    return result.json();
+}
+
+export async function fetchCartItems() {
+    const response = await fetch(`${backendURL}/cart`);
+    return response.json();
+}
+
+export async function removeFromCart(cartItemId) {
+    const result = await fetch(`${backendURL}/cart/${cartItemId}`, {
+        method: "DELETE"
+    });
+    return result.json();
+}
