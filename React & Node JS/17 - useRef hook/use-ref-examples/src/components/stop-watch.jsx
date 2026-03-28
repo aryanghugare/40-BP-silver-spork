47ede878eb39e27aa99e0d72b911de8986564173
Gaurav Sen <gaurav.s3n@gmail.com>
2026-03-28 13:07:12 +0530

    added comments

--- React & Node JS/17 - useRef hook/use-ref-examples/src/components/stop-watch.jsx
+++ React & Node JS/17 - useRef hook/use-ref-examples/src/components/stop-watch.jsx
@@ -1,5 +1,10 @@
 import { useRef, useState } from "react";
 export default function StopWatch() {
+  // useRef - value is persisted between re-renders
+  // you need to read and write on the current values. Ex - timerId.current = 0;
+  // updating a ref value will not trigger a re-render.
+  // do not use this value on the UI. If you need to show something on the UI - useState hook
+  // should be used
   const [time, setTime] = useState(0);
   const timerId = useRef(null);
   function start() {
@@ -9,6 +14,7 @@ export default function StopWatch() {
   }
   function stop() {
     clearInterval(timerId.current);
+    timerId.current = undefined;
   }
 
   return (
@@ -17,6 +23,7 @@ export default function StopWatch() {
       <footer>
         <button onClick={start}>Start</button>
         <button onClick={stop}>Stop</button>
+        {/* <p>TimerId:{timerId.current}</p> */}
       </footer>
     </article>
   );
