12d1ec54f8c5753b2d501384db8b8a56367bf6e8
Gaurav Sen <gaurav.s3n@gmail.com>
2026-03-28 13:33:02 +0530

    cat image slider example

--- React & Node JS/17 - useRef hook/use-ref-examples/src/App.jsx
+++ React & Node JS/17 - useRef hook/use-ref-examples/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import StopWatch from "./components/stop-watch";
 import Focus from "./components/focus";
+import ImageSlider from "./components/image-slider";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
     <>
       <StopWatch />
       <Focus />
+      <ImageSlider />
     </>
   );
 }
---
+++ React & Node JS/17 - useRef hook/use-ref-examples/src/components/image-slider.jsx
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+import "./image-slider.css";
+export default function ImageSlider() {
+  const containerRef = useRef(null);
+  function scrollToImage(index) {
+    const imageItems = containerRef.current.querySelectorAll(".image-item");
+    imageItems[index].scrollIntoView({
+      behavior: "smooth",
+      inline: "nearest",
+      block: "start",
+    });
+  }
+  return (
+    <>
+      <header>
+        <button onClick={() => scrollToImage(0)}>Neo</button>
+        <button onClick={() => scrollToImage(1)}>Millie</button>
+        <button onClick={() => scrollToImage(2)}>Bella</button>
+      </header>
+      <ul ref={containerRef} className="image-gallery">
+        <li className="image-item">
+          <img src="https://placecats.com/neo/300/200"></img>
+        </li>
+        <li className="image-item">
+          <img src="https://placecats.com/millie/200/200"></img>
+        </li>
+        <li className="image-item">
+          <img src="https://placecats.com/bella/199/200"></img>
+        </li>
+      </ul>
+    </>
+  );
+}
