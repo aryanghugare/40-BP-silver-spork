959de35f06cf7a0c6050ae857cb8b46e56259f8d
Gaurav Sen <gaurav.s3n@gmail.com>
2026-03-29 14:37:24 +0530

    Completed till tailwind setup

--- React & Node JS/18 - Authentication Project/auth-frontend/src/App.jsx
+++ React & Node JS/18 - Authentication Project/auth-frontend/src/App.jsx
@@ -6,7 +6,10 @@ function App() {
 
   return (
     <>
-      <h1>App is running</h1>
+      <article className="flex flex-col gap-4 border-2 w-sm p-4 mx-auto">
+        <h1 className="text-2xl">This is the heading</h1>
+        <p className="text-gray-500 dark:text-slate-700">Some context related to the title</p>
+      </article>
     </>
   );
 }
