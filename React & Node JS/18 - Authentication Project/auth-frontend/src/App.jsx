d40ef7e32b4148c09963f47aed84b5a2698f7186
Gaurav Sen <gaurav.s3n@gmail.com>
2026-04-01 18:03:34 +0530

    basic routing setup

--- React & Node JS/18 - Authentication Project/auth-frontend/src/App.jsx
+++ React & Node JS/18 - Authentication Project/auth-frontend/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router";
+import Login from "./pages/login/login";
+import Profile from "./pages/profile/profile";
+import Signup from "./pages/signup/signup";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <article className="flex flex-col gap-4 border-2 w-sm p-4 mx-auto">
-        <h1 className="text-2xl">This is the heading</h1>
-        <p className="text-gray-500 dark:text-slate-700">Some context related to the title</p>
-      </article>
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={<Profile />} />
+    </Routes>
   );
 }
 
