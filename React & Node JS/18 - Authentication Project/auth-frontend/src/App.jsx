0191955f33e69731a7ae63940fa1b13d43ca2523
Gaurav Sen <gaurav.s3n@gmail.com>
2026-04-01 18:33:36 +0530

    Implemented login form layout and users route layout

--- React & Node JS/18 - Authentication Project/auth-frontend/src/App.jsx
+++ React & Node JS/18 - Authentication Project/auth-frontend/src/App.jsx
@@ -4,14 +4,16 @@ import { Route, Routes } from "react-router";
 import Login from "./pages/login/login";
 import Profile from "./pages/profile/profile";
 import Signup from "./pages/signup/signup";
+import RegistrationLayout from "./components/registration-layout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/user" element={<RegistrationLayout />}>
+        <Route index element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Route>
+
       <Route path="/" element={<Profile />} />
     </Routes>
   );
--- React & Node JS/18 - Authentication Project/auth-frontend/src/pages/login/login.jsx
+++ React & Node JS/18 - Authentication Project/auth-frontend/src/pages/login/login.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <form className="bg-zinc-100 flex flex-col justify-center items-center gap-4 w-sm p-4 rounded-2xl shadow-xl">
+      <header className="text-2xl">
+        <h1>Login</h1>
+      </header>
+      <fieldset className="flex gap-4">
+        <label htmlFor="username">Username</label>
+        <input className="border rounded-xl" type="text" id="username" name="username" />
+      </fieldset>
+      <fieldset className="flex gap-4">
+        <label htmlFor="password">Password</label>
+        <input type="text" id="password" name="password" className="border rounded-xl" />
+      </fieldset>
+      <fieldset className="flex gap-4 items-center">
+        <input type="checkbox" name="rememberMe" id="rememberMe" className="size-4" />
+        <label htmlFor="rememberMe">Remember me</label>
+      </fieldset>
+      <footer>
+        <button className="bg-black p-1 rounded text-white">Login</button>
+      </footer>
+    </form>
+  );
 }
